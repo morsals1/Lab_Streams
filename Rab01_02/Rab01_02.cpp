6ca255426a4e876113a31d47b30914893276bc1c
morsals1 <157448960+morsals1@users.noreply.github.com>
2025-02-09 22:49:07 +0500

    Add files via upload

--- Rab01_02/Rab01_02.cpp
+++ Rab01_02/Rab01_02.cpp
@@ -2,74 +2,77 @@
 #include <Windows.h>
 #include <cstdio>
 
-int positions[10] = { 0 };
-bool check_winner = false;
-int winner_id = -1;
+const int TRACK_LENGTH = 50;
+const int MAX_TURTLES = 10;
+
+char race_tracks[MAX_TURTLES][TRACK_LENGTH + 1];
 FILE* logFile;
+int winner_id = -1;
 
-DWORD WINAPI Turtle_races(LPVOID iNum)
-{
-    int id = (int)iNum;
+DWORD WINAPI Turtle_races(LPVOID param) {
+    int id = (int)(uintptr_t)param;
     int current_position = 0;
-    int step_count = 0;
-
     srand(GetTickCount() + id * 100);
 
-    while (current_position < 50)
-    {
+    while (current_position < TRACK_LENGTH - 1) {
         int step = rand() % 3;
         current_position += step;
-        step_count++;
-
-        if (current_position >= 50) {
-            break;
+        if (current_position >= TRACK_LENGTH - 1) {
+            current_position = TRACK_LENGTH - 1;
         }
+
+        race_tracks[id - 1][current_position] = 'T';
 
         std::cout << "Поток " << id << " шаг " << step << " позиция " << current_position << "\n";
 
-        fprintf(logFile, "Поток %d, шаг %d, позиция %d\n", id, step, current_position);
-        fflush(logFile);
+        fopen_s(&logFile, "race_log.txt", "a");
+        if (logFile) {
+            fprintf(logFile, "Поток %d, шаг %d, позиция %d\n", id, step, current_position);
+            fclose(logFile);
+        }
 
-        Sleep(1000);
-        positions[id - 1] = current_position;
+        Sleep(1000 + rand() % 2000);
 
-        if (current_position == 49 && !check_winner) {
-            check_winner = true;
+        if (current_position >= TRACK_LENGTH - 1 && winner_id == -1) {
             winner_id = id;
-            break;
+        }
+
+        if (current_position - step >= 0) {
+            race_tracks[id - 1][current_position - step] = ' ';
         }
     }
 
     return 0;
 }
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Russian");
+
     int turtle_sum;
-
-    if (fopen_s(&logFile, "race_log.txt", "w") != 0) {
-        std::cerr << "Ошибка открытия файла!" << std::endl;
-        return 1;
-    }
-
-    HANDLE threads[10];
-    DWORD threadID[10];
-
-    std::cout << "Введите кол-во черепах от 5 до 10\n";
+    std::cout << "Введите количество черепах (от 5 до 10): ";
     std::cin >> turtle_sum;
 
-    if (turtle_sum < 5 || turtle_sum > 10) {
-        std::cout << "Неверное число черепах! Введите число от 5 до 10.\n";
-        fclose(logFile);
+    if (turtle_sum < 5 || turtle_sum > MAX_TURTLES) {
+        std::cerr << "Неверное число черепах! Введите от 5 до 10.\n";
         return 1;
     }
 
-    for (int i = 0; i < turtle_sum; i++) {
-        threads[i] = CreateThread(NULL, 0, Turtle_races, (LPVOID)(i + 1), 0, &threadID[i]);
+    fopen_s(&logFile, "race_log.txt", "w");
+    if (logFile) fclose(logFile);
 
+    for (int i = 0; i < turtle_sum; i++) {
+        for (int j = 0; j < TRACK_LENGTH; j++) {
+            race_tracks[i][j] = ' ';
+        }
+        race_tracks[i][TRACK_LENGTH] = '\0';
+    }
+
+    HANDLE threads[MAX_TURTLES];
+
+    for (int i = 0; i < turtle_sum; i++) {
+        threads[i] = CreateThread(NULL, 0, Turtle_races, (LPVOID)(uintptr_t)(i + 1), 0, NULL);
         if (threads[i] == NULL) {
-            fclose(logFile);
+            std::cerr << "Ошибка создания потока " << i + 1 << "\n";
             return GetLastError();
         }
     }
@@ -80,15 +83,14 @@ int main()
         CloseHandle(threads[i]);
     }
 
-    if (check_winner) {
-        std::cout << "Победитель: Поток с номером " << winner_id << "\n";
-        fprintf(logFile, "Победитель: Поток с номером %d\n", winner_id);
-    }
-    else {
-        std::cout << "Нет победителя.\n";
-        fprintf(logFile, "Нет победителя.\n");
+    fopen_s(&logFile, "race_log.txt", "a");
+    if (logFile) {
+        if (winner_id != -1) {
+            std::cout << "Победитель: Поток " << winner_id << "\n";
+            fprintf(logFile, "Победитель: Поток %d\n", winner_id);
+        }
+        fclose(logFile);
     }
 
-    fclose(logFile);
     return 0;
 }
